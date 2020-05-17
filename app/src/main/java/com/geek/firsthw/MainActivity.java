9bd5457d68870820daee96dd324294dd2eac9e64
Ivan Ognev <Ognev-dmitrov@mail.ru>
2020-05-17 14:15:19 +0300

    completeHW1

--- app/src/main/java/com/geek/firsthw/MainActivity.java
+++ app/src/main/java/com/geek/firsthw/MainActivity.java
@@ -3,12 +3,41 @@ package com.geek.firsthw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
+    private boolean newText;
+    private TextView firstMsg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        setListenerOnBtn();
     }
+
+    private void setListenerOnBtn() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(newText){
+                    firstMsg.setText("Is True");
+                    newText = false;
+                }else {
+                    firstMsg.setText("Is False");
+                    newText = true;
+                }
+            }
+        });
+    }
+
+    public void initViews(){
+        firstMsg= findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+    }
+
+
 }
