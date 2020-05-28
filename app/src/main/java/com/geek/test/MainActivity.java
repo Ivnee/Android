b99be1a76fc7c38171d981439acfe726de9e8698
Ivan Ognev <Ognev-dmitrov@mail.ru>
2020-05-28 14:08:31 +0300

    completeDZ3and4

--- app/src/main/java/com/geek/test/MainActivity.java
+++ app/src/main/java/com/geek/test/MainActivity.java
@@ -1,10 +1,12 @@
 package com.geek.test;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -12,9 +14,14 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button ok,clear;
-    private CheckBox windSpeed;
+    public static String checkBoxKey = "checkBoxKey";
+    public static String cityNameTextView = "cityName";
+    private int requestCodeToOpenNewPage = 10;
+    private Button ok, clear;
+    private CheckBox windSpeedCheckBox;
     private EditText editCity;
+    private static final String TAG = "myLogs";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,19 +29,23 @@ public class MainActivity extends AppCompatActivity {
         initComponents();
         addListener();
     }
-    private void initComponents(){
-     ok = findViewById(R.id.ok);
-     clear = findViewById(R.id.clear);
-     windSpeed = findViewById(R.id.checkWindSpeed);
-     editCity = findViewById(R.id.find_city);
+
+    private void initComponents() {
+        ok = findViewById(R.id.ok);
+        clear = findViewById(R.id.clear);
+        windSpeedCheckBox = findViewById(R.id.checkWindSpeed);
+        editCity = findViewById(R.id.find_city);
     }
-    private void addListener(){
+
+    private void addListener() {
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent("com.geek.test.NewPage");
-                startActivity(intent);
-                Toast.makeText(getBaseContext(),getString(R.string.Info_about) + editCity.getText(),Toast.LENGTH_SHORT ).show();
+                intent.putExtra(checkBoxKey, windSpeedCheckBox.isChecked());
+                intent.putExtra(cityNameTextView,String.valueOf(editCity.getText()));
+                startActivityForResult(intent, requestCodeToOpenNewPage);
+                Toast.makeText(getBaseContext(), getString(R.string.Info_about) + editCity.getText(), Toast.LENGTH_SHORT).show();
             }
         });
         clear.setOnClickListener(new View.OnClickListener() {
@@ -44,4 +55,59 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == requestCodeToOpenNewPage) {
+            if (resultCode == RESULT_OK && data != null) {
+                Toast.makeText(getBaseContext(), data.getStringExtra(NewPage.tempDataKey), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    /*
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, getString(R.string.on_start));
+        Toast.makeText(getBaseContext(), getString(R.string.on_start), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, getString(R.string.on_stop));
+        Toast.makeText(getBaseContext(), R.string.on_stop, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, getString(R.string.on_restart));
+        Toast.makeText(getBaseContext(), R.string.on_restart, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, getString(R.string.on_resume));
+        Toast.makeText(getBaseContext(), R.string.on_resume, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, getString(R.string.on_pause));
+        Toast.makeText(getBaseContext(), R.string.on_pause, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, getString(R.string.on_destroy));
+        Toast.makeText(getBaseContext(), R.string.on_destroy, Toast.LENGTH_SHORT).show();
+    }
+*/
 }
--- app/src/main/java/com/geek/test/NewPage.java
+++ app/src/main/java/com/geek/test/NewPage.java
@@ -1,9 +1,13 @@
 package com.geek.test;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,27 +15,89 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.util.Random;
+
 public class NewPage extends AppCompatActivity {
-    private TextView temp,today,tomorrow,date;
-    private Button back;
+    public static final String tempDataKey = "temp data key";
+    private static boolean checkWindSpeed = false;
+    private TextView temp, today, tomorrow, date, windSpdView, checkCityView;
+    private Button back,info;
+    Random rd = new Random();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_page);
         initComponents();
+        showReceivedData();
+        windSpeedInfo();
+        setTemp();
+        setCity();
+        addListenerForCheckInfo();
+        addListenerToComeback();
     }
-    public void initComponents(){
+
+    public void initComponents() {
         temp = findViewById(R.id.temp);
         today = findViewById(R.id.today);
+        info = findViewById(R.id.infoBtn);
         tomorrow = findViewById(R.id.tomorrow);
         date = findViewById(R.id.date);
+        checkCityView = findViewById(R.id.city_intent);
+        windSpdView = findViewById(R.id.wind_spd);
     }
-    public void addListenerForReturn(){
-        back.setOnClickListener(new View.OnClickListener() {
+    public void addListenerForCheckInfo(){
+        info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onBackPressed();
+                Intent infoIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://in-dmitrov.ru/"));
+                startActivity(infoIntent);
             }
         });
+    }
+
+    public void addListenerToComeback() {
+        findViewById(R.id.back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent tempIntent = new Intent();
+                String tempText = String.valueOf(temp.getText());
+                tempIntent.putExtra(tempDataKey, tempText);
+                setResult(RESULT_OK, tempIntent);
+                finish();
+            }
+        });
+    }
+
+    private void showReceivedData() {
+        Intent checkIntent = getIntent();
+        checkWindSpeed = checkIntent.getBooleanExtra(MainActivity.checkBoxKey, false);
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void windSpeedInfo() {
+        if (checkWindSpeed) {
+            int speed = rd.nextInt(10);
+            windSpdView.setText(getString(R.string.wind_speed_msg) + String.valueOf(speed));
+
+        }
+    }
+
+    private void setCity(){
+        Intent cityTextIntent = getIntent();
+        String cityName = cityTextIntent.getStringExtra(MainActivity.cityNameTextView);
+        checkCityView.setText(cityName);
+    }
+
+    private void setTemp() {
+        int t = rd.nextInt(40) - 20;
+        if (t > 0) {
+            int color = ContextCompat.getColor(getBaseContext(), R.color.tempColor);
+            temp.setText("+" + String.valueOf(t));
+        } else {
+            temp.setText(String.valueOf(t));
+            int color = ContextCompat.getColor(getBaseContext(), R.color.minusTempColor);
+            temp.setTextColor(color);
+        }
     }
 }
